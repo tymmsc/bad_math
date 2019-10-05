476eec8f117c032c019551c9e04114dac13679f7
Eduardo Iglesias <eriglesias@ufl.edu>
2019-10-05 12:08:40 -0400

    add working button to remove trailing digits

---
+++ Assets/Scripts/RemoveTrailingDigit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveTrailingDigit : OperatorScript
+{
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public override bool DoOp(Equation inputEq)
+    {
+        string newRight = "";
+        string newLeft = "";
+        //The new strings are the old ones with one leading zero removed 
+        int rightLength = inputEq.rightSide.Length;
+        int leftLength = inputEq.leftSide.Length;
+        if(inputEq.rightSide[rightLength -1] == inputEq.leftSide[leftLength-1]  &&
+            rightLength > 1 && leftLength > 1)
+        {
+            newRight = inputEq.rightSide.Substring(0,rightLength -1);
+            newLeft = inputEq.leftSide.Substring(0, leftLength -1);
+            inputEq.leftSide = newLeft;
+            inputEq.rightSide = newRight;
+            return true;
+        }
+        return false;
+
+
+ 
+
+        //left side = right side
+        //for each chara
+    }
+}
